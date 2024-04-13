5b76f089bf6afdac4dba6f187f0b08c0feef43dd
PromiseUdo <info.promiseudo@gmail.com>
2024-04-13 04:07:09 +0100

    update

--- server/dist/config/db.js
+++ server/dist/config/db.js
@@ -13,6 +13,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const connectDB = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const mongoURI = process.env.MONGO_URI;
