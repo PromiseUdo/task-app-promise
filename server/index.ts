93810dcfce0ce4dc08d68d0bc722d6bf52c47261
PromiseUdo <info.promiseudo@gmail.com>
2024-04-13 02:58:57 +0100

    vercel config

--- server/index.ts
+++ server/index.ts
@@ -9,11 +9,11 @@ const port = process.env.PORT || 5000;
 
 import userRoutes from "./routes/userRoutes";
 import taskRoutes from "./routes/taskRoutes";
-import path from "path";
+import path, { join } from "path";
 
 connectDB();
 const app = express();
-
+app.use(express.static(path, join(__dirname, "/dist")));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
